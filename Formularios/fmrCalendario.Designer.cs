5ceae2f57729afc15d7f24fe4f9a134e0039e8cb
user <user@USER-PC>
2020-11-22 11:04:19 -0500

    datetime

---
+++ Formularios/fmrCalendario.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Formularios
+{
+    partial class fmrCalendario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnTime = new System.Windows.Forms.Button();
+            this.txtAllos = new System.Windows.Forms.TextBox();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(137, 42);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(225, 86);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 13);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.VisibleChanged += new System.EventHandler(this.lblFecha_VisibleChanged);
+            // 
+            // btnTime
+            // 
+            this.btnTime.Location = new System.Drawing.Point(36, 155);
+            this.btnTime.Name = "btnTime";
+            this.btnTime.Size = new System.Drawing.Size(75, 23);
+            this.btnTime.TabIndex = 2;
+            this.btnTime.Text = "Tiempo";
+            this.btnTime.UseVisualStyleBackColor = true;
+            this.btnTime.Click += new System.EventHandler(this.btnTime_Click);
+            // 
+            // txtAllos
+            // 
+            this.txtAllos.Location = new System.Drawing.Point(332, 202);
+            this.txtAllos.Name = "txtAllos";
+            this.txtAllos.Size = new System.Drawing.Size(100, 20);
+            this.txtAllos.TabIndex = 3;
+            // 
+            // txtDias
+            // 
+            this.txtDias.Location = new System.Drawing.Point(332, 256);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(100, 20);
+            this.txtDias.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tiempo en años";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tiempo en dias";
+            // 
+            // fmrCalendario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.txtAllos);
+            this.Controls.Add(this.btnTime);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "fmrCalendario";
+            this.Text = "fmrCalendario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnTime;
+        private System.Windows.Forms.TextBox txtAllos;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Formularios/fmrCalendario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios
+{
+    public partial class fmrCalendario : Form
+    {
+        public fmrCalendario()
+        {
+            InitializeComponent();
+        }
+
+        private void lblFecha_VisibleChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnTime_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = dateTimePicker1.Value;
+            lblFecha.Text = fecha.ToString();
+            DateTime Actual = DateTime.Today;
+            int Allos = Actual.Year - dateTimePicker1.Value.Year;
+            int Dias = ((Actual.Year - dateTimePicker1.Value.Year) * 365) + ((Actual.Month - dateTimePicker1.Value.Month) * 31)+ Actual.Day - dateTimePicker1.Value.Day;
+            txtAllos.Text = Allos.ToString() + " Tiempo en años";
+            txtDias.Text = Dias.ToString() + " Tiempo en Días";
+        }
+    }
+}
