50b7d88895dd817b5caeb338296132e3a9153cfe
user <user@USER-PC>
2020-11-22 01:36:11 -0500

    Verificar Nombre y edad

---
+++ Formularios/frmNombreEdad.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Formularios
+{
+    partial class frmNombreEdad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtVNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(174, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(174, 127);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingresa nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingresa edad";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(297, 143);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtVNombre
+            // 
+            this.txtVNombre.Enabled = false;
+            this.txtVNombre.Location = new System.Drawing.Point(390, 96);
+            this.txtVNombre.Name = "txtVNombre";
+            this.txtVNombre.Size = new System.Drawing.Size(365, 20);
+            this.txtVNombre.TabIndex = 5;
+            // 
+            // frmNombreEdad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtVNombre);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "frmNombreEdad";
+            this.Text = "frmNombreEdad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtVNombre;
+    }
+}
---
+++ Formularios/frmNombreEdad.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formularios
+{
+    public partial class frmNombreEdad : Form
+    {
+        public frmNombreEdad()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+            int edad = int.Parse(txtEdad.Text);
+
+            if (edad >= 15 && edad <= 80)
+            {
+                if (edad >= 15 && edad <= 17)
+                {
+
+                    txtVNombre.Text = txtNombre.Text + " Eres menor de edad";
+                    txtVNombre.Visible = true;
+                }
+                else if (edad >= 18 && edad <= 25)
+                {
+
+                    txtVNombre.Text = txtNombre.Text + " Puedes votar en las elecciones 2021";
+                    txtVNombre.Visible = true;
+                }
+                else if (edad >= 26 && edad <= 30)
+                {
+
+                    txtVNombre.Text = txtNombre.Text + " Ya debes casarte";
+                    txtVNombre.Visible = true;
+                }
+                else if (edad >= 31 && edad <= 50)
+                {
+
+                    txtVNombre.Text = txtNombre.Text + " Trabaja, duerme y disfruta ";
+                    txtVNombre.Visible = true;
+                }
+                else if (edad > 50)
+                {
+
+                    txtVNombre.Text = txtNombre.Text + " Ya casi estas en la tercera edad";
+                    txtVNombre.Visible = true;
+                }
+                else if (edad > 80)
+                {
+                    txtVNombre.Text = txtNombre.Text + " Ya casi estas en la tercera edad";
+                    txtVNombre.Visible = true;
+                }
+            }
+            else if(edad>80)
+            {
+                txtVNombre.Text = txtNombre.Text + " Usted ya es de la tercera edad";
+                txtVNombre.Visible = true;
+            }
+
+        }
+    }
+}
