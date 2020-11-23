cc593d075a3eae8b1e5c8aaebfc85aeca5107545
user <user@USER-PC>
2020-11-23 10:26:24 -0500

    Manu

--- Formularios/Program.cs
+++ Formularios/Program.cs
@@ -16,7 +16,7 @@ namespace Formularios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmMenu());
         }
     }
 }
